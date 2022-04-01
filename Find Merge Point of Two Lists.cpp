5992cfec8dcff942dd89c4787b7a7d08d48091dd
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-04-01 22:08:38 +0530

    Create Find Merge Point of Two Lists.cpp

---
+++ Find Merge Point of Two Lists.cpp
@@ -0,0 +1,17 @@
+int findMergeNode(SinglyLinkedListNode* headA, SinglyLinkedListNode* headB) {
+    while (headA) {
+        SinglyLinkedListNode *tmp = headA->next;
+        headA->next = NULL;
+        headA = tmp;
+    }
+
+    while (headB) {
+        if (headB->next == NULL) {
+            return headB->data;
+        }
+        headB = headB->next;
+    }
+
+    return 0;
+
+}
